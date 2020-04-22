da092a1db75eadc02c7fd205b17835b472c12a2f
Dan <danyatimachov@gmail.com>
2020-04-22 18:09:09 +0300

    Finished console widget-like weather tracker

--- src/lections/src/weatherTracker/main.go
+++ src/lections/src/weatherTracker/main.go
@@ -4,15 +4,29 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"reflect"
+	"strconv"
 	"time"
+
+	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep/wav"
 )
 
-const cAPIKey = "90b34303c90966d9988b014c6388d0f4"
+const (
+	cAPIKey       = "90b34303c90966d9988b014c6388d0f4"
+	celsiusKelvin = 273.15
+	delaySec      = 300
+	maxPolling    = 480
+)
 
 var (
 	myClient = &http.Client{Timeout: 10 * time.Second}
 	city     string
+	prevdata *cityData
 	data     *cityData
 )
 
@@ -22,7 +36,7 @@ type weatherData struct {
 	MinKelvins  float32 `json:"temp_min"`
 	MaxKelvins  float32 `json:"temp_max"`
 	Pressure    int     `json:"pressure"`
-	Humidity    int     `json:"hunidity"`
+	Humidity    int     `json:"humidity"`
 }
 
 type wind struct {
@@ -60,14 +74,60 @@ func getJSON(city string) *cityData {
 	return data
 }
 
+func display(data *cityData) {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05 Monday"))
+	fmt.Printf("Absolute temperature: %fK(%fºC)\n", data.Main.TempKelvins, data.Main.TempKelvins-celsiusKelvin)
+	fmt.Printf("Feels like: %fK\n", data.Main.FeelsLike)
+	fmt.Printf("Min/Max temperature: %fK / %fK\n", data.Main.MinKelvins, data.Main.MaxKelvins)
+	fmt.Printf("Pressure: %dkPa\n", data.Main.Pressure)
+	fmt.Printf("Humidity: %d%%\n", data.Main.Humidity)
+	fmt.Printf("City: %s", data.Name)
+}
+
 func main() {
+	err := os.RemoveAll("src/weatherTracker/data")
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.MkdirAll("src/weatherTracker/data", os.ModeDir)
+
 	fmt.Print("Enter city to check weather: ")
 	fmt.Scan(&city)
 
-	getJSON(city)
-	data = getJSON(city)
+	for longpoll := 0; longpoll < maxPolling; longpoll++ {
+		getJSON(city)
+		prevdata = data
+		data = getJSON(city)
 
-	file, _ := json.MarshalIndent(data, "", " ")
+		display(data)
 
-	_ = ioutil.WriteFile("src/weatherTracker/data/data.json", file, 0644)
+		if prevdata != nil && data != nil {
+			if !reflect.DeepEqual(data, prevdata) {
+				f, err := os.Open("src/weatherTracker/assets/beep.wav")
+				if err != nil {
+					log.Fatal(err)
+				}
+				streamer, format, err := wav.Decode(f)
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer streamer.Close()
+
+				speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+				speaker.Play(streamer)
+			}
+		}
+
+		file, _ := json.MarshalIndent(data, "", " ")
+		err := ioutil.WriteFile("src/weatherTracker/data/"+strconv.FormatInt(time.Now().UnixNano(), 10)+"data.json", file, 0644)
+
+		if err != nil {
+			panic(err)
+		}
+
+		time.Sleep(time.Second * delaySec)
+	}
 }
